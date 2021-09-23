1db9363df9eb585c02a84cb2d575736027286c62
mewcoder <1010953107@qq.com>
2021-09-24 00:18:29 +0800

    feat: 🎸 编译成render

--- jg-vue-202109/vue2-stage/src/init.js
+++ jg-vue-202109/vue2-stage/src/init.js
@@ -3,5 +3,28 @@ export function initMixin(Vue) {
     const vm = this;
     vm.$options = options;
     initState(vm); //初始化数据
+
+    if (vm.$options.el) {
+      //将数据挂载到这个模块上
+      vm.$mount(vm.$options.el);
+    }
+  };
+
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    const options = vm.$options;
+
+    el = document.querySelector(el);
+
+    if (!options.render) {
+      let template = options.template;
+      if (!template && el) {
+        template = el.outerHTML;
+        let render = compileToFunctions(template);
+        options.render = render;
+      }
+    }
+
+    //把模块转换成 对应的渲染函数
   };
 }
